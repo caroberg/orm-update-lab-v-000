b19581cf18b70e1d202a19b79aba9b7e389a857a
Caroline Berg <carolineeberg@gmail.com>
2019-01-20 17:18:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,16 +54,21 @@ class Student
   end
 
   def self.find_by_name(name)
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE name = ?
-      LIMIT 1
-    SQL
+    sql = <<-SQL 
+      SELECT * 
+      FROM students 
+      WHERE name = ? 
+      LIMIT 1 
+    SQL 
 
-    DB[:conn].execute(sql, name).map do |row|
+    DB[:conn].execute(sql, name).map do |row| 
       self.new_from_db(row)
-    end.first
+    end.first 
+  end
+
+  def update 
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    
   end
 
 end
